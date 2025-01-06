657f1d2348ed442616b53c1663733065817720ed
romainsame <romain.s@h2altitude.com>
2025-01-06 09:22:53 +0100

    templates campaign

--- wallam/urls.py
+++ wallam/urls.py
@@ -17,13 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from core import urls as core_url
-from core.views import login_view, register_view
+from core.views import login_view, register_view, logout_view
 from campaign import urls as campaign_url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(core_url)),
-    path('campaign/', include(campaign_url)),
+    path('', include(campaign_url)),
     path('connexion', login_view, name='login'),
+    path('deconnexion', logout_view, name='logout'),
     path('enregistrement', register_view, name='register'),
 ]
