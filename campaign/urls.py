4605509cd318cf051beed0e390df5bb9c486a896
romainsameh2a <romain.s@h2altitude.com>
2025-09-27 01:51:18 +0100

    paiement taramoney version

--- campaign/urls.py
+++ campaign/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("campaign/create-campaign/", views.create_campaign_view, name="create_campaign"),
     path("campaign/edit-campaign/<int:pk>/", views.edit_campaign_view, name="edit_campaign"),
     path("campaign/<int:pk>/", views.campaign_detail_view, name="campaign_detail"),
-    path("campaign/<int:pk>/donate/", views.campaign_donate, name="campaign_donate"),
+    path("campaign/<int:pk>/donate/", views.campaign_donate_view, name="campaign_donate"),
     path("campaigns", views.campaign_list, name="campaign_list")
 ]
